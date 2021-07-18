e855f7c7d12783d2c16ac035a057d6c8f49f94e1
Ritik-S <87600655+Ritik-S@users.noreply.github.com>
2021-07-18 16:05:08 +0530

    Add files via upload

---
+++ square_root_of_a_number_using_binary_search.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<math.h>
+float  square_root(int n,int k)
+{
+
+
+       float left=0,right=n,ans=0;
+
+
+       while(left<right)
+       {
+
+       	  float mid=(left+right)/2;
+
+         	mid=mid*pow(10,k);
+          mid=(int)(mid);
+
+         	mid/=pow(10,k);
+
+
+
+       if((float)(mid*mid)<=n)
+       	{
+       	     	ans=mid;
+
+       		  	left=mid*pow(10,k);
+       		  	left=(int)left;
+       		  	left++;
+       		  	left/=pow(10,k);
+
+       	}
+       	else
+       	{
+       	     	right=mid*pow(10,k);
+       	     	right=(int)right;
+       		  	right/=pow(10,k);
+       	}
+       }
+
+            return ans;
+
+}
+int main()
+{
+   	int n,k ;
+	  scanf("%d%d",&n,&k);
+   	float ans=square_root(n,k);
+
+    printf("%g",ans);
+}
